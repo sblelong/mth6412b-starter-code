731641258783cb44456059bdc946b1edccbc90f0
Sacha Benarroch-Lelong <sblelong@zaclys.net>
2024-10-06 18:02:46 -0400

    create utils.jl

---
+++ src/phase2/utils.jl
@@ -0,0 +1,20 @@
+"""
+    sort_edges(edges)
+
+Trie un dictionnaire d'arétes dans l'ordre croissant ou décroissant de leurs poids. Utilise un tri par fusion.
+
+Arguments
+    edges::Dict{String,Edge}: le dictionnaire d'arêtes à trier
+"""
+function sort_edges(edges::Dict{String,Edge})
+    function merge_dicts(keys::Vector, values::Vector)
+        # TODO
+        return nothing
+    end
+
+
+    function merge_sort_dict(keys::Vector, values::Vector)
+        # TODO
+       return nothing
+    end
+end
