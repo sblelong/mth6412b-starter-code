ad02032051def746d2da40cc6f2a028bc5289a69
Sacha Benarroch-Lelong <sblelong@zaclys.net>
2024-10-07 20:15:21 -0400

    lint + doc

--- src/phase2/connected.jl
+++ src/phase2/connected.jl
@@ -2,9 +2,9 @@ export Tree, Forest
 
 """Type representant un arbre comme l'identifiant d'un parent et une taille d'arbre.
 
-L'identifiant d'un parent dérive de l'identifiant du noeud d'un graphe. 
+L'identifiant d'un parent dérive de l'identifiant du noeud d'un graphe.
 La taille d'un arbre est définie comme le nombre d'arbres qui ont cet arbre comme parent.
-Ce type est principalement utile pour le type "Forest".
+Ce type est principalement utile pour le type `Forest`.
 """
 mutable struct Tree
   parent_id::String
@@ -14,34 +14,50 @@ end
 """Type representant une forêt comme un ensemble d'identifiants de noeuds pointant vers des arbres.
 
 Les identifiants dérivent des identifiants des noeuds d'un graphe.
-A partir de l'identifiant du noeud d'un graphe, cette structure permet de trouver l'arbre associé dans le dictionnaire "trees".
+A partir de l'identifiant du noeud d'un graphe, cette structure permet de trouver l'arbre associé dans le dictionnaire `trees``.
 Si l'identifiant d'un noeud pointe vers un arbre dont le parent a le même identifiant, alors ce noeud est une "racine". 
 Les "racines" de l'arbre sont utiles pour les fusionner et pour vérifier l'existence de cycles.
-Voir la documentation de la fonction "merge(forest::Forest, root_id1::String, root_id2::String)" pour plus de détails.
 
-Le nombre de "racines" contenue dans la forêt est également stockée dans "num_roots".
+Voir la documentation de la fonction [`merge`](@ref) pour plus de détails.
+
+Le nombre de "racines" contenue dans la forêt est également stockée dans l'attribut `num_roots`.
 """
 mutable struct Forest
   trees::Dict{String,Tree}
   num_roots::Int64
 end
 
-"""Constructeur du type "Forest".
-
-Initialement, chaque arbre associé à chaque identifiant de noeud est une "racine" de taille 1.
 """
-function Forest(G::Graph{T, U}) where{T, U}
-  trees = Dict{String, Tree}()
+  Forest(G)
+
+Initialise une forêt de composantes connexes à partir d'un graphe. Un arbre de taille 1 est créé par noeud du graphe. Cette fonction est conçue pour servir à l'initialisation de l'algorithme de Kruskal.
+
+# Arguments
+- G (`Graph``): le graphe à partir duquel construire une forêt de composantes connexes
+"""
+function Forest(G::Graph{T,U}) where {T,U}
+  trees = Dict{String,Tree}()
   for (node_id, node) in G.nodes
     trees[node_id] = Tree(node_id, 1)
   end
   return Forest(trees, length(G.nodes))
 end
 
-"""Fonction permettant de trouver la "racine" de l'arbre associé à l'identifiant d'un noeud.
+"""
+	find(forest, node_id)
 
-La fonction itère de parent en parent jusqu'à trouver un identifiant dont le parent est lui-même.
-La fonction renvoie l'identifiant de la "racine" trouvée.
+Retourne la racine de l'arbre associé au noeud d'identifiant `node_id` dans la forêt `forest`. Itère de parent en parent jusqu'à trouver un identifiant dont le parent est lui-même.
+
+# Arguments
+- forest (`Forest`): forêt dans laquelle rechercher l'arbre auquel est rattaché le noeud
+- `node_id` (String): identifiant du noeud à rechercher dans la forêt
+
+# Exemple
+```julia-repl
+julia> find("24", forest)
+Tree("7", 12)
+# Le noeud d'identifiant "24" est contenu dans l'arbre de la forêt dont la racine est d'identifiant "7", et de taille 12.
+```
 """
 function find(forest::Forest, node_id::String)
   trees = forest.trees
@@ -54,12 +70,20 @@ function find(forest::Forest, node_id::String)
   return head_id
 end
 
-"""Fonction permettant de fusionner deux arbres à partir de deux identifiants qui ont la propriété d'être des "racines".
-
-La fusion s'opère en redéfinissant le parent d'une "racine" par l'autre "racine".
-Pour choisir quelle racine prend l'autre racine comme enfant, la taille des arbres associé est comparée ; l'arbre de plus grande taille englobe l'autre.
 """
-function merge(forest::Forest, root_id1::String, root_id2::String)
+	merge!(forest, root_id1, root_id2)
+
+Fonction permettant de fusionner deux arbres à partir de deux identifiants qui ont la propriété d'être des "racines". La fusion s'opère en redéfinissant le parent d'une "racine" par l'autre "racine". Pour choisir quelle racine prend l'autre racine comme enfant, la taille des arbres associé est comparée ; l'arbre de plus grande taille englobe l'autre. Ce choix est justifié [ici](https://en.wikipedia.org/wiki/Disjoint-set_data_structure#Union_by_size).
+
+# Arguments
+- `forest` (`Forest`): forêt dans laquelle se trouvent les 2 arbres à fusionner
+- `root_id1` (`String`): identifiant de la racine du premier arbre participant à la fusion
+- `root_id2` (`String`): identifiant de la racine du second arbre participant à la fusion
+
+# Type de retour
+Aucun : fonction *in-place*.
+"""
+function merge!(forest::Forest, root_id1::String, root_id2::String)
   trees = forest.trees
   if trees[root_id1].size > trees[root_id2].size
     trees[root_id2].parent_id = root_id1
@@ -68,5 +92,4 @@ function merge(forest::Forest, root_id1::String, root_id2::String)
     trees[root_id1].parent_id = root_id2
     trees[root_id2].size = trees[root_id2].size + trees[root_id1].size
   end
-  
 end
--- src/phase2/kruskal.jl
+++ src/phase2/kruskal.jl
@@ -7,15 +7,15 @@ La fonction renvoie un tuple contenant le coût et une liste des arêtes formant
 
 Si le graphe n'est pas connexe, une erreur est renvoyée.
 """
-function kruskal(G::Graph{T, U}) where{T, U}
+function kruskal(G::Graph{T,U}) where {T,U}
 
   ## Construct the initial forest
   F = Forest(G)
   cost = 0
-  edges = Edge{U}[] 
+  edges = Edge{U}[]
 
   ## Order the edges
-  sorted = sort(collect(G.edges), by = x -> x[2].data)
+  sorted = sort(collect(G.edges), by=x -> x[2].data)
 
   k = 1
   while F.num_roots > 1 && k ≤ length(sorted)
@@ -30,14 +30,14 @@ function kruskal(G::Graph{T, U}) where{T, U}
     if root_node1 ≠ root_node2 # L'arête n'ajoute pas de cycle.
       F.num_roots = F.num_roots - 1
       cost = cost + edge.data
-      merge(F, root_node1, root_node2)
+      merge!(F, root_node1, root_node2)
       push!(edges, edge)
     end
 
     k = k + 1
   end
 
-  # Toutes les arêtes ont été explorées mais il reste deux composantes connexes ne pouvant pas être fusionnées. 
+  # Toutes les arêtes ont été explorées mais il reste deux composantes connexes ne pouvant pas être fusionnées.
   if k > length(sorted)
     error("Kruskal: Graph is not connected.")
   end
