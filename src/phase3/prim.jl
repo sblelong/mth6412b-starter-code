f0de2d19a17b75f1dacba98cbc0dd0b987c8b457
Maxence Gollier <mgollier45@gmail.com>
2024-10-26 15:30:24 -0400

    add documentation for prim and queues

--- src/phase3/prim.jl
+++ src/phase3/prim.jl
@@ -1,13 +1,31 @@
 export prim
 
+"""
+  prim(G)
+
+Implémentation de l'algorithme de Prim, le premier noeud est choisi aléatoirement parmi les noeuds du graphe.
+Si le graphe n'est pas connexe, une erreur est renvoyée.
+
+# Arguments
+- G(`Graph`): le graphe sur lequel on exécute l'algorithme de Prim
+"""
 function prim(G::Graph{T, U}) where{T, U}
   init_node_id = rand(keys(G.nodes))
   return prim(G, init_node_id)
 end
 
+"""
+  prim(G, init_node_id)
+
+Implémentation de l'algorithme de Prim.
+Si le graphe n'est pas connexe, une erreur est renvoyée.
+
+# Arguments
+- G(`Graph`): le graphe sur lequel on exécute l'algorithme de Prim
+- init_node_id (`String`): l'identifiant du noeud initial
+"""
 function prim(G::Graph{T,U}, init_node_id::String) where {T, U}
 
-	## TODO check init_node in G + add function to only call node id
   edges = Edge{U}[]
 	min_weights = PrimPriorityQueue{U}()
 	min_weights.order = "min"
@@ -18,24 +36,24 @@ function prim(G::Graph{T,U}, init_node_id::String) where {T, U}
 		min_weights.items[node] = node == init_node_id ? 0 : typemax(U)
 	end
 	
-	cost = 0
+	cost = U(0)
 	while !is_empty(min_weights)
 
-		u, weight = popfirst!(min_weights)
-		if weight == typemax(U)
-			error("Prim: Graph is not connected.")
-		end
-		cost += weight
-		for edge in adjacency[u]
-      weight = edge.data
-      v = edge.node1_id == u ? edge.node2_id : edge.node1_id
-			if haskey(min_weights.items, v)
-				if weight < min_weights.items[v] 
-          push!(edges,edge)
-					min_weights.items[v] = weight 
-				end
-			end
-		end
+  u, weight = popfirst!(min_weights)
+  if weight == typemax(U)
+    error("Prim: Graph is not connected.")
+  end
+  cost += weight
+  for edge in adjacency[u]
+    weight = edge.data
+    v = edge.node1_id == u ? edge.node2_id : edge.node1_id
+    if haskey(min_weights.items, v)
+      if weight < min_weights.items[v] 
+        push!(edges,edge)
+        min_weights.items[v] = weight 
+      end
+    end
+  end
 
 	
 	end
--- src/phase3/priority_queue.jl
+++ src/phase3/priority_queue.jl
@@ -5,34 +5,74 @@ export PrimPriorityQueue
 """Type abstrait dont d'autres types de files dériveront."""
 abstract type AbstractQueue{U} end
 
+"""
+  popfirst!(q)
 
-"""Retire et renvoie l'objet du début de la file."""
+Retire et renvoie le premier élément d'une file abstraite.
+"""
 popfirst!(q::AbstractQueue) = popfirst!(q.items)
 
-"""Indique si la file est vide."""
+"""
+  is_empty(q)
+
+Indique si une file abstraite est vide.
+"""
 is_empty(q::AbstractQueue) = length(q.items) == 0
 
-"""Donne le nombre d'éléments sur la file."""
+"""
+  length(q)
+
+Retourne le nombre d'éléments d'une file abstraite.
+"""
 length(q::AbstractQueue) = length(q.items)
 
-"""Affiche une file."""
+"""
+  show(q)
+
+Affiche les éléments d'une file abstraite.
+"""
 show(q::AbstractQueue) = show(q.items)
 
-"""File de priorité."""
+"""Type représentant une implémentation d'une file de priorité permettant une implémentation efficace de l'algorithme de Prim.
+
+Cette structure de donnée stocke des identifiants de noeuds ainsi que des priorités dont le type est paramétrique dans le dictionnaire `items`. 
+L'ordre de supression des éléments de la file est indiqué par `order` dont la valeur par défaut est `"max"`.
+Lors d'un appel à `popfirst!`, si l'ordre est max, la paire identifiant-priorité ayant la priorité la plus élevée est supprimée.
+Si l'ordre choisi est "min" alors c'est la paire ayant la plus petite priorité qui est supprimée.
+"""
 mutable struct PrimPriorityQueue{U} <: AbstractQueue{U}
     items::Dict{String, U}
     order::String
 end
 
-"""Ajoute `item` à la fin de la file `s`."""
+"""
+  push!(q, name, priority)
+
+Rajoute une paire identifiant priorité à une file de priorité de Prim. 
+
+#Arguments
+- q (`PrimPriorityQueue`): La file de priorité permettant une implémentation efficace de l'algorithme de Prim.
+- name (`String`): Un identifiant de noeud qu'on ajoute à la file de priorité.
+- priority (`U`): La priorité de l'identifiant dans la file dont le type est celui des poids des arêtes du graphe que l'algorithme de Prim résout.
+"""
 function push!(q::PrimPriorityQueue, name::String, priority::U) where{U}
     q.items[name] = priority
     q
 end
 
+"""
+  PrimPriorityQueue{U}()
+
+Initialise une file de priorité de Prim de type paramétrique U vide.
+"""
 PrimPriorityQueue{U}() where{U} = PrimPriorityQueue(Dict{String, U}(), "max")
 
-"""Retire et renvoie l'élément ayant la plus haute priorité."""
+"""
+  popfirst!(q)
+
+Si l'ordre de la file (stocké dans `q.order`) est `"max"`, retire et renvoie un élément de la file ayant la plus grande priorité.
+Si l'ordre de la file (stocké dans `q.order`) est `"min"`, retire et renvoie un élément de la file ayant la plus petite priorité.
+"""
 function popfirst!(q::PrimPriorityQueue)
   highest_key, highest = first(q.items)
   for (key, value) in q.items
