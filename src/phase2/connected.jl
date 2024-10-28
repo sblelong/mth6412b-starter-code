3f5bf302ce4b578dda81b63736b82ff97e423b6a
Sacha Benarroch-Lelong <sblelong@zaclys.net>
2024-10-28 11:48:49 -0400

    add rank union on kruskal

--- src/phase2/connected.jl
+++ src/phase2/connected.jl
@@ -4,12 +4,31 @@ export Tree, Forest
 
 L'identifiant d'un parent dérive de l'identifiant du noeud d'un graphe.
 La taille d'un arbre est définie comme le nombre d'arbres qui ont cet arbre comme parent.
+Le rang d'un arbre est un attribut utile pour la procédure d'union de composantes connexes par le rang.
+Un seul attribut parmi la taille et le rang n'est pas nul.
 Ce type est principalement utile pour le type `Forest`.
 """
 mutable struct Tree
   parent_id::String
-  size::Int64
+  size::Union{Int64,Nothing}
+  rank::Union{Int64,Nothing}
 end
+
+"""
+  Tree(parent_id, value; mode)
+
+Initialise un arbre à partir de l'identifiant de sa racine et d'une valeur de taille et de rang (option choisie par l'argumment `mode`).
+
+# Arguments
+- `parent_id` (`String`): l'identifiant du noeud constituant la racine de l'arbre
+- `value` (`Int64`): la valeur numérique de la taille ou du rang
+- `mode` (`String`="size"): ("size" ou "rank"). L'attribut qui doit être initialisé à la valeur `value` (la taille ou le rang).
+"""
+function Tree(parent_id::String, value::Int64; mode::String="size")
+  mode in ["size", "rank"] || error("When initializing a Tree, the `mode` argument should be 'size' or 'rank'.")
+  mode == "size" ? Tree(parent_id, value, nothing) : Tree(parent_id, nothing, value)
+end
+
 
 """Type representant une forêt comme un ensemble d'identifiants de noeuds pointant vers des arbres.
 
@@ -20,7 +39,7 @@ Les "racines" de l'arbre sont utiles pour les fusionner et pour vérifier l'exis
 
 Voir la documentation de la fonction [`merge`](@ref) pour plus de détails.
 
-Le nombre de "racines" contenue dans la forêt est également stockée dans l'attribut `num_roots`.
+Le nombre de "racines" contenue dans la forêt est également stocké dans l'attribut `num_roots`.
 """
 mutable struct Forest
   trees::Dict{String,Tree}
@@ -28,17 +47,19 @@ mutable struct Forest
 end
 
 """
-  Forest(G)
+  Forest(G; mode)
 
-Initialise une forêt de composantes connexes à partir d'un graphe. Un arbre de taille 1 est créé par noeud du graphe. Cette fonction est conçue pour servir à l'initialisation de l'algorithme de Kruskal.
+Initialise une forêt de composantes connexes à partir d'un graphe. Un arbre de taille 1 ou de rang 0 est créé par noeud du graphe. Cette fonction est conçue pour servir à l'initialisation de l'algorithme de Kruskal.
 
 # Arguments
-- G (`Graph``): le graphe à partir duquel construire une forêt de composantes connexes
+- `G` (`Graph`): le graphe à partir duquel construire une forêt de composantes connexes
+- `mode` (`String="size"`) : (`"size"` ou `"rank"`). L'attribut à initialiser dans chaque arbre créé
 """
-function Forest(G::Graph{T,U}) where {T,U}
+function Forest(G::Graph{T,U}; mode::String="size") where {T,U}
   trees = Dict{String,Tree}()
+  mode in ["size", "rank"] || error("When initializing a Forest, the `mode` argument should be 'size' or 'rank'.")
   for (node_id, node) in G.nodes
-    trees[node_id] = Tree(node_id, 1)
+    trees[node_id] = mode == "size" ? Tree(node_id, 1) : Tree(node_id, 0, mode="rank")
   end
   return Forest(trees, length(G.nodes))
 end
@@ -70,26 +91,53 @@ function find(forest::Forest, node_id::String)
   return head_id
 end
 
+import Base.merge!
 """
-	merge!(forest, root_id1, root_id2)
+	merge!(forest, root_id1, root_id2; mode)
 
-Fonction permettant de fusionner deux arbres à partir de deux identifiants qui ont la propriété d'être des "racines". La fusion s'opère en redéfinissant le parent d'une "racine" par l'autre "racine". Pour choisir quelle racine prend l'autre racine comme enfant, la taille des arbres associé est comparée ; l'arbre de plus grande taille englobe l'autre. Ce choix est justifié [ici](https://en.wikipedia.org/wiki/Disjoint-set_data_structure#Union_by_size).
+Fonction permettant de fusionner deux arbres à partir de deux identifiants qui ont la propriété d'être des "racines". La fusion s'opère en redéfinissant le parent d'une "racine" par l'autre "racine".
+
+Pour choisir quelle racine prend l'autre racine comme enfant, 2 possibilités sont proposées.
+
+### [Union par la taille](https://en.wikipedia.org/wiki/Disjoint-set_data_structure#Union_by_size)
+La taille des arbres associés est comparée ; l'arbre de plus grande taille englobe l'autre. Cela nécessite que les attributs `size` des arbres de la forêt ne soient pas nuls.
+
+### Union par le rang
+Les rangs sont comparés et incrémentés selon la politique classique d'union par le rang.
 
 # Arguments
 - `forest` (`Forest`): forêt dans laquelle se trouvent les 2 arbres à fusionner
 - `root_id1` (`String`): identifiant de la racine du premier arbre participant à la fusion
 - `root_id2` (`String`): identifiant de la racine du second arbre participant à la fusion
+- `mode` (`String="size"`): (`"size"` ou `"rank"`). Indique le critère retenu pour l'union.
 
 # Type de retour
 Aucun : fonction *in-place*.
 """
-function merge!(forest::Forest, root_id1::String, root_id2::String)
+function merge!(forest::Forest, root_id1::String, root_id2::String; mode::String="size")
   trees = forest.trees
-  if trees[root_id1].size > trees[root_id2].size
-    trees[root_id2].parent_id = root_id1
-    trees[root_id1].size = trees[root_id1].size + trees[root_id2].size
+
+  if mode == "size"
+    if trees[root_id1].size > trees[root_id2].size
+      trees[root_id2].parent_id = root_id1
+      trees[root_id1].size = trees[root_id1].size + trees[root_id2].size
+    else
+      trees[root_id1].parent_id = root_id2
+      trees[root_id2].size = trees[root_id2].size + trees[root_id1].size
+    end
+  elseif mode == "rank"
+    new_root = trees[root_id1].rank > trees[root_id2].rank ? root_id1 : root_id2
+    new_child = trees[root_id1].rank > trees[root_id2].rank ? root_id2 : root_id1
+
+    # Si les rangs sont égaux, c'est la seconde qui devient parent de la première et son rang augmente de 1.
+    if trees[root_id1].rank == trees[root_id2].rank
+      trees[new_root].rank += 1
+    end
+    trees[new_child].parent_id = new_root
   else
-    trees[root_id1].parent_id = root_id2
-    trees[root_id2].size = trees[root_id2].size + trees[root_id1].size
+    error("When performing merge on connected components, the `mode` argument should be 'size' or 'rank'.")
   end
+
 end
+
+export merge!
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,26 +1,26 @@
-using STSP,Test
+using STSP, Test
 
 @testset "Lecture Notes Graph" begin
   nodes = Node{Int64}[]
   for letter in 'a':'i'
-    push!(nodes,Node(string(letter),0))
+    push!(nodes, Node(string(letter), 0))
   end
 
   edges = Edge{Int64}[]
-  push!(edges, Edge("a", "b", 4 ))
-  push!(edges, Edge("b", "c", 8 ))
-  push!(edges, Edge("c", "d", 7 ))
-  push!(edges, Edge("d", "e", 9 ))
-  push!(edges, Edge( "e", "f", 10))
-  push!(edges, Edge( "d", "f", 14))
-  push!(edges, Edge("f", "c", 4 ))
-  push!(edges, Edge("f", "g", 2 ))
-  push!(edges, Edge("c", "i", 2 ))
-  push!(edges, Edge("g", "i", 6 ))
-  push!(edges, Edge("h", "i", 7 ))
-  push!(edges, Edge("h", "g", 1 ))
-  push!(edges, Edge( "h", "b", 11))
-  push!(edges, Edge("h", "a", 8 ))
+  push!(edges, Edge("a", "b", 4))
+  push!(edges, Edge("b", "c", 8))
+  push!(edges, Edge("c", "d", 7))
+  push!(edges, Edge("d", "e", 9))
+  push!(edges, Edge("e", "f", 10))
+  push!(edges, Edge("d", "f", 14))
+  push!(edges, Edge("f", "c", 4))
+  push!(edges, Edge("f", "g", 2))
+  push!(edges, Edge("c", "i", 2))
+  push!(edges, Edge("g", "i", 6))
+  push!(edges, Edge("h", "i", 7))
+  push!(edges, Edge("h", "g", 1))
+  push!(edges, Edge("h", "b", 11))
+  push!(edges, Edge("h", "a", 8))
 
   G = Graph("KruskalLectureNotesTest", nodes, edges)
 
@@ -29,69 +29,93 @@ using STSP,Test
   @test edges[1].data == 1
   @test edges[end].data == 9
 
-  cost, edges = prim(G, "a")
-
-  @test cost == 37
-  @test edges[1].data == 4
-  @test edges[end].data == 9
-
-  edges = Edge{Float32}[]
-  push!(edges, Edge("a", "b", Float32(4) ))
-  push!(edges, Edge("b", "c", Float32(8) ))
-  push!(edges, Edge("c", "d", Float32(7) ))
-  push!(edges, Edge("d", "e", Float32(9) ))
-  push!(edges, Edge( "e", "f", Float32(10)))
-  push!(edges, Edge( "d", "f", Float32(14)))
-  push!(edges, Edge("f", "c", Float32(4) ))
-  push!(edges, Edge("f", "g", Float32(2) ))
-  push!(edges, Edge("c", "i", Float32(2) ))
-  push!(edges, Edge("g", "i", Float32(6) ))
-  push!(edges, Edge("h", "i", Float32(7) ))
-  push!(edges, Edge("h", "g", Float32(1) ))
-  push!(edges, Edge( "h", "b", Float32(11)))
-  push!(edges, Edge("h", "a", Float32(8) ))
-
-  G = Graph("KruskalLectureNotesTest", nodes, edges)
-
-  cost, edges = kruskal(G)
-  @test typeof(cost) == Float32
-  @test cost == 37
-
-  cost, edges = prim(G, "a")
-
-  @test typeof(cost) == Float32
-  @test cost == 37
-
-
   edges = Edge{Int64}[]
-  push!(edges, Edge("a", "b", 4 ))
-  push!(edges, Edge("c", "d", 7 ))
-  push!(edges, Edge("d", "e", 9 ))
-  push!(edges, Edge( "e", "f", 10))
-  push!(edges, Edge( "d", "f", 14))
-  push!(edges, Edge("f", "c", 4 ))
-  push!(edges, Edge("f", "g", 2 ))
-  push!(edges, Edge("c", "i", 2 ))
-  push!(edges, Edge("g", "i", 6 ))
-  push!(edges, Edge( "h", "b", 11))
-  push!(edges, Edge("h", "a", 8 ))
+  push!(edges, Edge("a", "b", 4))
+  push!(edges, Edge("c", "d", 7))
+  push!(edges, Edge("d", "e", 9))
+  push!(edges, Edge("e", "f", 10))
+  push!(edges, Edge("d", "f", 14))
+  push!(edges, Edge("f", "c", 4))
+  push!(edges, Edge("f", "g", 2))
+  push!(edges, Edge("c", "i", 2))
+  push!(edges, Edge("g", "i", 6))
+  push!(edges, Edge("h", "b", 11))
+  push!(edges, Edge("h", "a", 8))
 
   G = Graph("NonConnectedKruskalLectureNotesTest", nodes, edges)
   let err = nothing
     try
-        kruskal(G)
+      kruskal(G)
     catch err
     end
     @test err isa Exception
     @test sprint(showerror, err) == "Kruskal: Graph is not connected."
   end
+end
 
-  let err = nothing
-    try
-        prim(G)
-    catch err
-    end
-    @test err isa Exception
-    @test sprint(showerror, err) == "Prim: Graph is not connected."
+@testset "AccelerationHeuristics" begin
+  # A Tree initialized with size mode as empty rank, and vice-versa.
+  size_tree = Tree("3", 1)
+  explicit_size_tree = Tree("3", 1, mode="size")
+  rank_tree = Tree("3", 0, mode="rank")
+
+  @test size_tree.size == explicit_size_tree.size
+  @test isnothing(size_tree.rank)
+  @test isnothing(rank_tree.size)
+
+  # Merge based on the rank
+  nodes = Node{Int64}[]
+  push!(nodes, Node("a", 0))
+  push!(nodes, Node("b", 0))
+  push!(nodes, Node("c", 0))
+
+  edges = Edge{Int64}[]
+  push!(edges, Edge("a", "b", 1))
+  push!(edges, Edge("b", "c", 1))
+
+  G = Graph("TestGraph", nodes, edges)
+  F = Forest(G, mode="rank")
+  # A merge based on the rank should yield the following results :
+  # 1. When merging 2 nodes with the same rank, one should increase
+  merge!(F, "a", "b", mode="rank")
+  # The rank of b should be 1, and a 0
+  @test F.trees["a"].parent_id == "b"
+  @test F.trees["b"].rank == 1
+  @test F.trees["a"].rank == 0
+
+  merge!(F, "b", "c", mode="rank")
+  # Ranks should not change
+  @test F.trees["c"].parent_id == "b"
+  @test F.trees["b"].rank == 1
+  @test F.trees["c"].rank == 0
+
+  # Kruskal with rank
+  nodes = Node{Int64}[]
+  for letter in 'a':'i'
+    push!(nodes, Node(string(letter), 0))
   end
+
+  edges = Edge{Int64}[]
+  push!(edges, Edge("a", "b", 4))
+  push!(edges, Edge("b", "c", 8))
+  push!(edges, Edge("c", "d", 7))
+  push!(edges, Edge("d", "e", 9))
+  push!(edges, Edge("e", "f", 10))
+  push!(edges, Edge("d", "f", 14))
+  push!(edges, Edge("f", "c", 4))
+  push!(edges, Edge("f", "g", 2))
+  push!(edges, Edge("c", "i", 2))
+  push!(edges, Edge("g", "i", 6))
+  push!(edges, Edge("h", "i", 7))
+  push!(edges, Edge("h", "g", 1))
+  push!(edges, Edge("h", "b", 11))
+  push!(edges, Edge("h", "a", 8))
+
+  G = Graph("KruskalLectureNotesTest", nodes, edges)
+
+  cost, edges = kruskal(G, mode="rank")
+
+  @test cost == 37
+  @test edges[1].data == 1
+  @test edges[end].data == 9
 end
