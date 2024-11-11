72ae11ff5dbb9463a2f2c1e904a191287422011a
Sacha Benarroch-Lelong <sblelong@zaclys.net>
2024-11-11 11:13:42 -0500

    fake commit to run CI

--- src/phase3/prim.jl
+++ src/phase3/prim.jl
@@ -9,7 +9,7 @@ Si le graphe n'est pas connexe, une erreur est renvoyée.
 # Arguments
 - G(`Graph`): le graphe sur lequel on exécute l'algorithme de Prim
 """
-function prim(G::Graph{T, U}) where{T, U}
+function prim(G::Graph{T,U}) where {T,U}
   init_node_id = rand(keys(G.nodes))
   return prim(G, init_node_id)
 end
@@ -24,44 +24,44 @@ Si le graphe n'est pas connexe, une erreur est renvoyée.
 - G(`Graph`): le graphe sur lequel on exécute l'algorithme de Prim
 - init_node_id (`String`): l'identifiant du noeud initial
 """
-function prim(G::Graph{T,U}, init_node_id::String) where {T, U}
+function prim(G::Graph{T,U}, init_node_id::String) where {T,U}
 
   edges = Edge{U}[]
-	min_weights = PrimPriorityQueue{U}()
-	min_weights.order = "min"
-	nodes = keys(G.nodes)
-	adjacency = G.adjacency
-  parents = Dict{String, Union{Edge{U}, Nothing}}()
+  min_weights = PrimPriorityQueue{U}()
+  min_weights.order = "min"
+  nodes = keys(G.nodes)
+  adjacency = G.adjacency
+  parents = Dict{String,Union{Edge{U},Nothing}}()
 
-	for node in nodes
-		min_weights.items[node] = node == init_node_id ? 0 : typemax(U)
+  for node in nodes
+    min_weights.items[node] = node == init_node_id ? 0 : typemax(U)
     parents[node] = nothing
-	end
-	
-	cost = U(0)
-	while !is_empty(min_weights)
-
-  u, weight = popfirst!(min_weights)
-  !isnothing(parents[u]) && push!(edges, parents[u])
-  
-  if weight == typemax(U)
-    error("Prim: Graph is not connected.")
   end
 
-  cost += weight
-  for edge in adjacency[u]
-    weight = edge.data
-    v = edge.node1_id == u ? edge.node2_id : edge.node1_id
-    if haskey(min_weights.items, v)
-      if weight < min_weights.items[v] 
-        min_weights.items[v] = weight 
-        parents[v] = edge
+  cost = U(0)
+  while !is_empty(min_weights)
+
+    u, weight = popfirst!(min_weights)
+    !isnothing(parents[u]) && push!(edges, parents[u])
+
+    if weight == typemax(U)
+      error("Prim: Graph is not connected.")
+    end
+
+    cost += weight
+    for edge in adjacency[u]
+      weight = edge.data
+      v = edge.node1_id == u ? edge.node2_id : edge.node1_id
+      if haskey(min_weights.items, v)
+        if weight < min_weights.items[v]
+          min_weights.items[v] = weight
+          parents[v] = edge
+        end
       end
     end
+
+
   end
-
-	
-	end
-
-	return cost, edges
+  # TODO
+  return cost, edges
 end
