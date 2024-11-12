a6a9cfd2e5bf004cb66b0655670c2fea41627379
Sacha Benarroch-Lelong <sblelong@zaclys.net>
2024-11-12 17:35:09 -0500

    add exploration.jl

--- src/phase4/exploration.jl
+++ src/phase4/exploration.jl
@@ -1,15 +1,6 @@
 export dfs
 
-function dfs(G::Graph{T}) for node in nodes(G)
-    visited(node) || dfs_visit(G, node)
+function dfs(G::Graph{T})
+    for node in nodes(G)
     end
-    return end
-    function dfs_visit(G::Graph{T}, node::MarkedNode{T})
-    set_visited!(node) # node devient la racine d'une nouvelle arborescence
-    for neighbor in neighbors(G, node)
-    visited(neighbor) && continue set_parent!(neighbor, node)
-    dfs_visit(G, neighbor)
-    end
-    return
-    
-    end
+end
