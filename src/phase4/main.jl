f6b249b4d118b7dc5033d464c4ff783dd7f38aa5
Sacha Benarroch-Lelong <sblelong@zaclys.net>
2024-11-14 11:53:33 -0500

    add phase4 output files

---
+++ src/phase4/main.jl
@@ -0,0 +1,30 @@
+using Logging
+
+function test_phase4(instance::String)
+    filename = "instances/stsp/" * instance
+    G = read_stsp(filename)
+    known_opt = get_optimal(instance)
+    test_phase4(G, known_opt)
+end
+
+function test_phase4(G::Graph{T,U}, known_opt::Float32) where {T,U}
+    Base.println("### Test des méthodes de la phase 4 sur l'instance ", G.name, " ###")
+
+    Base.println("Coût optimal connu | ", known_opt)
+
+    # Test de RSL
+    rsl_tour = rsl(G)
+    rsl_cost = tour_cost(G, rsl_tour)
+    Base.println("1. RSL")
+    Base.println("Coût de la tournée proposée | ", rsl_cost)
+    Base.println("RSL/opt (%) | ", rsl_cost / known_opt * 100)
+
+    Base.println("\n")
+
+    # Test de HK
+    #=  hk_tour = held_karp(G)
+    hk_cost = tour_cost(G, hk_tour)
+    Base.println("2. Held & Karp")
+    Base.println("Coût de la tournée proposée | ", hk_cost)
+    Base.println("HK/opt (%) | ", hk_cost / known_opt * 100) =#
+end
---
+++ src/phase4/report.jl
@@ -0,0 +1,25 @@
+### A Pluto.jl notebook ###
+# v0.20.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ a761fd2c-a29d-11ef-030c-5b7d55f07fe9
+md"""
+Maxence Gollier, Sacha Benarroch-Lelong
+
+MTH6412B, Polytechnique Montréal, Automne 2024
+## Projet voyageur de commerce - Phase 4
+
+Notre code est disponible sur [ce dépôt GitHub](https://github.com/sblelong/mth6412b-starter-code). Il est organisé selon une structure de projet Julia. L'environnement associé est nommé `STSP`.
+
+La quatrième phase est consacrée à l'implémentation d'algorithmes pour la confection de tournées minimales dans des graphes complets.
+"""
+
+
+# ╔═╡ 6852139e-9169-4d90-a1ca-89a95de62da1
+
+
+# ╔═╡ Cell order:
+# ╟─a761fd2c-a29d-11ef-030c-5b7d55f07fe9
+# ╠═6852139e-9169-4d90-a1ca-89a95de62da1
